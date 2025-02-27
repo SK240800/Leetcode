3f5ff17e1f971ceb64efecb19e408144cbec611d
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-02-27 13:50:00 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.70%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        prev = [1]
-        for i in range(1, rowIndex + 1):
-            curr = [1] * (i + 1)
-            for j in range(1, i):
-                curr[j] = prev[j - 1] + prev[j]
-            prev = curr
-        return prev
+        # prev = [1]
+        # for i in range(1, rowIndex + 1):
+        #     curr = [1] * (i + 1)
+        #     for j in range(1, i):
+        #         curr[j] = prev[j - 1] + prev[j]
+        #     prev = curr
+        # return prev
+        row = [1] * (rowIndex + 1)
+        for i in range(2, rowIndex + 1):
+            for j in range(i - 1, 0, -1):
+                row[j] += row[j - 1]
+        
+        return row
         
