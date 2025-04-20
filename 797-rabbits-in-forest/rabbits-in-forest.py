ada897540808ce91bb2b2a87f6bcbb9f1c692830
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-20 12:00:22 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.93%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numRabbits(self, answers):
+        """
+        :type answers: List[int]
+        :rtype: int
+        """
+        count = collections.Counter(answers)
+        return sum(-v % (k+1) + v for k, v in count.iteritems())
