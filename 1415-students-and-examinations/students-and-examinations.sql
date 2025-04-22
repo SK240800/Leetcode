df92c594fd0e04f704d3673aece33841ecab9b0a
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-22 02:03:05 -0500

    Time: 1004 ms (67.25%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,14 +1,10 @@
-with crs as(
-    select s.student_id, s.student_name, sub.subject_name
-    from Students s
-    cross join Subjects sub
-)
-select c.student_id,c.student_name,c.subject_name,IFNULL(grouped.attended_exams, 0) AS attended_exams
-from crs c
-left join (
-    select student_id,subject_name, COUNT(*) as attended_exams
+select s.student_id,s.student_name,sb.subject_name,coalesce(gp.attended_exams,0)as attended_exams
+from Students s
+cross join Subjects sb
+left join(
+    select student_id,subject_name,count(*) as attended_exams
     from Examinations
-    group by student_id, subject_name
-) grouped
-on c.student_id = grouped.student_id AND c.subject_name = grouped.subject_name
-ORDER BY c.student_id, c.subject_name;
+    group by student_id,subject_name
+) gp
+on s.student_id=gp.student_id and sb.subject_name=gp.subject_name
+ORDER by s.student_id,sb.subject_name
