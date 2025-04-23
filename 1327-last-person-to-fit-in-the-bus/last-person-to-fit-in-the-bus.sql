4ec94f47f5d4ababa89d760fce9a02c96267209a
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-23 15:40:18 -0500

    Time: 784 ms (86.44%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,7 @@
 select person_name
-from(select person_name,sum(weight) over(order by turn) as cs
+from(select person_name,sum(weight) over(order by turn rows between unbounded preceding and current row) as cs
 from Queue
-order by turn) as r
+) as r
 where cs<=1000
 order by cs desc
 limit 1
