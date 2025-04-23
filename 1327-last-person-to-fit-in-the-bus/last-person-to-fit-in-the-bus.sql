5f5a2e2ad008f062ae854c0da8248ee05ebc5c1c
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-23 15:38:43 -0500

    Time: 841 ms (75.41%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,5 +1,5 @@
 select person_name
-from(select person_name,turn, weight ,sum(weight) over(order by turn) as cs
+from(select person_name,sum(weight) over(order by turn) as cs
 from Queue
 order by turn) as r
 where cs<=1000
