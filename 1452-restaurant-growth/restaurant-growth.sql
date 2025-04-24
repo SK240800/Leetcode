4e11ab6bbf9a19d30239804072c2d4a25d700323
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-24 14:44:28 -0500

    Time: 354 ms (82.27%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,8 +1,6 @@
 select distinct visited_on,
-        sum(amount) over w as amount,
-        round((sum(amount) over w)/7, 2) as average_amount
-from customer
-WINDOW w AS ( 
-            order by visited_on
-            range between interval 6 day PRECEDING and current row)
-            Limit 6, 999
+sum(amount)over w as amount,
+round(sum(amount) over w /7 ,2) as average_amount
+from Customer
+window w as (order by visited_on range between interval 6 day preceding and current row) limit 6,999;
+
