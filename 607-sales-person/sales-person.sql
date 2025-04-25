46b4cc29ec2ad3063eeab54f2a01a7cf1dbc4c0a
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-24 23:53:56 -0500

    Time: 1343 ms (87.59%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,12 +1,21 @@
-select j as name from(select s.name as j,r.name
-from SalesPerson s
-left join (
-    select o.sales_id,name
-    from Orders o
-    left join Company c
-    on o.com_id=c.com_id
-    ) as r
-on s.sales_id=r.sales_id
-group by s.name
-having r.name!='RED' or r.name is null) as e
+-- select j as name from(select s.name as j,r.name
+-- from SalesPerson s
+-- left join (
+--     select o.sales_id,name
+--     from Orders o
+--     left join Company c
+--     on o.com_id=c.com_id
+--     ) as r
+-- on s.sales_id=r.sales_id
+-- group by s.name
+-- having r.name!='RED' or r.name is null) as e
 
+select s.name
+from salesperson s
+where s.sales_id not in (
+    select o.sales_id
+    from orders o
+    left join Company c 
+    on o.com_id = c.com_id
+    where c.name='RED'
+    )
