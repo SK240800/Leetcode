0637c964bc070b3b3617f6a43c5700c2420a3daf
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-25 17:03:14 -0500

    Time: 438 ms (80.63%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,6 +1,9 @@
-with cte as (select count(delivery_id) as imd
-from Delivery
-where order_date = customer_pref_delivery_date)
+-- with cte as (select count(delivery_id) as imd
+-- from Delivery
+-- where order_date = customer_pref_delivery_date)
 
-select round(imd*100/count(delivery_id),2) as immediate_percentage
-from cte,Delivery
+-- select round(imd*100/count(delivery_id),2) as immediate_percentage
+-- from cte,Delivery
+
+select round(100*avg(order_date=customer_pref_delivery_date),2) as immediate_percentage
+from Delivery
