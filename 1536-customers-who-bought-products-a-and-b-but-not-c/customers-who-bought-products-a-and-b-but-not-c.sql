ee5155dd37929e7037f672387f57c82cb8282ea0
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-24 21:01:35 -0500

    Time: 683 ms (67.82%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,15 +1,24 @@
 # Write your MySQL query statement below
-select distinct f.customer_id,f.customer_name
-from (
-select o.customer_id,c.customer_name
-from Orders o
-join Customers c
-on o.customer_id=c.customer_id
-order by customer_name) as f
-where (f.customer_id, 'A') in (
-    select customer_id,product_name
-    from Orders) and (f.customer_id, 'B') in (
-    select customer_id,product_name
-    from Orders) and (f.customer_id, 'C')  not in (
-    select customer_id,product_name
-    from Orders)
+-- select distinct f.customer_id,f.customer_name
+-- from (
+-- select o.customer_id,c.customer_name
+-- from Orders o
+-- join Customers c
+-- on o.customer_id=c.customer_id
+-- order by customer_name) as f
+-- where (f.customer_id, 'A') in (
+--     select customer_id,product_name
+--     from Orders) and (f.customer_id, 'B') in (
+--     select customer_id,product_name
+--     from Orders) and (f.customer_id, 'C')  not in (
+--     select customer_id,product_name
+--     from Orders)
+
+select c.customer_id, customer_name
+from customers c
+left join orders o on c.customer_id = o.customer_id
+group by c.customer_id
+having SUM(product_name='A') > 0
+    AND SUM(product_name='B') > 0
+    AND SUM(product_name='C') = 0
+order by customer_id;
