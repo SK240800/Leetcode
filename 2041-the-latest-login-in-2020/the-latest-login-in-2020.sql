698a8372f8f5ebcc6ea21e353adf071a1cc3519f
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-25 16:29:30 -0500

    Time: 653 ms (73.49%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-select user_id, time_stamp as last_stamp from(
-    select user_id ,time_stamp,dense_rank() over (partition by user_id order by time_stamp desc) as r
-    from Logins
-    having year(time_stamp)=2020) as r
-where r=1
-order by user_id
+-- select user_id, time_stamp as last_stamp from(
+--     select user_id ,time_stamp,dense_rank() over (partition by user_id order by time_stamp desc) as r
+--     from Logins
+--     having year(time_stamp)=2020) as r
+-- where r=1
+-- order by user_id
+
+select distinct user_id,first_value(time_stamp)over(partition by user_id order by time_stamp desc) as last_stamp
+from Logins
+where year(time_stamp)=2020
+
