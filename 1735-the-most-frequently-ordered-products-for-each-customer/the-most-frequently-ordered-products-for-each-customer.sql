bb8f2cd5247221bbd582cdeafe0cee89b1193254
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-26 16:52:15 -0500

    Time: 2677 ms (51.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select m.customer_id,m.product_id,p.product_name
+from(select customer_id,product_id,count(product_id),dense_rank()over(partition by customer_id order by count(product_id) desc) as dr
+from Orders
+group by customer_id,product_id) m
+left join Products p
+on p.product_id=m.product_id
+where m.dr=1
