86f4a070e265293b78bda013af5a4b049ad52efb
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-26 14:10:23 -0500

    Time: 838 ms (78.39%) | Memory: 0B (100.00%) - LeetSync

--- 1481-students-with-invalid-departments/students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
-select s.id, s.name
-from Students s
-left join Departments d
-on d.id=s.department_id
-where d.name is null
+-- select s.id, s.name
+-- from Students s
+-- left join Departments d
+-- on d.id=s.department_id
+-- where d.name is null
+
+select id , name
+from Students
+where department_id not in (select id from Departments)
