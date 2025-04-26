3be9d30a86021e59d54d63d91608409553c75185
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-26 10:43:26 -0500

    Time: 601 ms (60.58%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1399-page-recommendations/page-recommendations.sql
@@ -0,0 +1,16 @@
+with cte as (select
+    (case 
+        when user1_id = 1 then user2_id
+        when user2_id = 1 then user1_id
+    end) as user_id
+from Friendship)
+
+select distinct page_id as recommended_page
+from cte 
+join Likes
+on cte.user_id=Likes.user_id
+where page_id not in (select page_id from Likes where user_id=1)
+
+
+
+
