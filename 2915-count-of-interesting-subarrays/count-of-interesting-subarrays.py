d5cf5d67900ad538b2d8a873b18e031d97c2ab90
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-25 21:26:34 -0500

    Time: 287 ms (7.69%) | Memory: 25.5 MB (38.46%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countInterestingSubarrays(self, nums, modulo, k):
+        """
+        :type nums: List[int]
+        :type modulo: int
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        cnt = Counter([0])
+        res = 0
+        prefix = 0
+        for i in range(n):
+            prefix += 1 if nums[i] % modulo == k else 0
+            res += cnt[(prefix - k + modulo) % modulo]
+            cnt[prefix % modulo] += 1
+        return res       
