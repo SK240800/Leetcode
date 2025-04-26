72000c1ee04d230438d41818177d44b62e60f299
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-26 14:19:33 -0500

    Time: 284 ms (81.52%) | Memory: 0B (100.00%) - LeetSync

--- 1438-find-the-team-size/find-the-team-size.sql
+++ 1438-find-the-team-size/find-the-team-size.sql
@@ -1,9 +1,12 @@
 # Write your MySQL query statement below
-with cte as (select team_id,count(employee_id) as size
-from Employee
-group by team_id)
+-- with cte as (select team_id,count(employee_id) as size
+-- from Employee
+-- group by team_id)
 
-select e.employee_id,c.size as team_size
-from Employee e
-left join cte c
-using(team_id)
+-- select e.employee_id,c.size as team_size
+-- from Employee e
+-- left join cte c
+-- using(team_id)
+
+select employee_id, count(team_id) over (partition by team_id ) as team_size
+from Employee
