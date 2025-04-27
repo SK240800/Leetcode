75e12fa917a3cb8018b70f639b6be5b11c71094a
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-04-26 22:48:24 -0500

    Time: 250 ms (72.68%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,16 +1,24 @@
-# Write your MySQL query statement below
-select employee_id
-from Employees
-where manager_id=1 and employee_id!=1
-union
-select employee_id
-from Employees
-where manager_id in(select employee_id
-from Employees where manager_id=1) and manager_id!=1
-union
-select employee_id
-from Employees
-where manager_id in (select employee_id
-from Employees
-where manager_id in(select employee_id
-from Employees where manager_id=1) and manager_id!=1)
+-- # Write your MySQL query statement below
+-- select employee_id
+-- from Employees
+-- where manager_id=1 and employee_id!=1
+-- union
+-- select employee_id
+-- from Employees
+-- where manager_id in(select employee_id
+-- from Employees where manager_id=1) and manager_id!=1
+-- union
+-- select employee_id
+-- from Employees
+-- where manager_id in (select employee_id
+-- from Employees
+-- where manager_id in(select employee_id
+-- from Employees where manager_id=1) and manager_id!=1)
+
+select e3.employee_id 
+from Employees e1
+join Employees e2
+on e2.manager_id=e1.employee_id
+join Employees e3
+on e3.manager_id=e2.employee_id
+where e1.manager_id=1 and e3.employee_id!=1
