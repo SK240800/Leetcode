b6c24836ed532fded0f4a48670d05957f75e5e0d
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-05-10 09:56:39 -0500

    Time: 888 ms (84.91%) | Memory: 26.1 MB (22.64%) - LeetSync

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minSum(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        sum1 = sum2 = 0
+        zero1 = zero2 = 0
+
+        for i in nums1:
+            sum1 += i
+            if i == 0:
+                sum1 += 1
+                zero1 += 1
+
+        for i in nums2:
+            sum2 += i
+            if i == 0:
+                sum2 += 1
+                zero2 += 1
+
+        if (zero1 == 0 and sum2 > sum1) or (zero2 == 0 and sum1 > sum2):
+            return -1
+
+        return max(sum1, sum2)        
