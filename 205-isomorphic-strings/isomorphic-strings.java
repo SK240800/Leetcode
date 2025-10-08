b4bbaa89b1eaa284954d271e03bf5d343a50d8e0
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-10-08 14:26:59 -0500

    Time: 24 ms (12.68%) | Memory: 42.2 MB (94.93%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,26 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        Map<Character, Character> mapST = new HashMap<>();
-        Map<Character, Character> mapTS = new HashMap<>();
-
-        char[] ss = s.toCharArray();
-        char[] ts = t.toCharArray();
-
-        for (int c = 0; c < s.length(); c++) {
-            char sc = ss[c];
-            char tc = ts[c];
-
-            // If mapping already exists, it must match
-            if (mapST.containsKey(sc) && mapST.get(sc) != tc) return false;
-            if (mapTS.containsKey(tc) && mapTS.get(tc) != sc) return false;
-
-            // Otherwise, store the new mapping
-            mapST.put(sc, tc);
-            mapTS.put(tc, sc);
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> assignedValues = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)) && map.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+            if (!map.containsKey(s.charAt(i)) && assignedValues.contains(t.charAt(i))) {
+                return false;
+            }
+            map.put(s.charAt(i), t.charAt(i));
+            assignedValues.add(t.charAt(i));
         }
-
         return true;
     }
 }
