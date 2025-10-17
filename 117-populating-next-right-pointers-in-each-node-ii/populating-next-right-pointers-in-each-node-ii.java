708124f22d08659640c2c3e2b2e8606fcc608d20
Sai Kiran Adlluru <144968759+SK240800@users.noreply.github.com>
2025-10-17 12:59:36 -0500

    Time: 1 ms (67.06%) | Memory: 44.5 MB (41.22%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -2,42 +2,17 @@ class Solution {
     public Node connect(Node root) {
         if (root == null) return root;
 
-        Node cur = root; // current level's first node
-        Node head = null; // head of the next level
-        Node prev = null; // previous node on the next level
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
 
-        // Traverse level by level
-        while (cur != null) {
-            // Iterate over current level
-            while (cur != null) {
-                // Connect left child
-                if (cur.left != null) {
-                    if (prev != null) {
-                        prev.next = cur.left;
-                    } else {
-                        head = cur.left;
-                    }
-                    prev = cur.left;
-                }
-
-                // Connect right child
-                if (cur.right != null) {
-                    if (prev != null) {
-                        prev.next = cur.right;
-                    } else {
-                        head = cur.right;
-                    }
-                    prev = cur.right;
-                }
-
-                // Move to next node in current level
-                cur = cur.next;
+        while (!q.isEmpty()) {
+            int level = q.size();
+            while (level-- > 0) {
+                Node curr = q.poll();
+                if (level != 0) curr.next = q.peek();
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
             }
-
-            // Move to the next level
-            cur = head;
-            head = null;
-            prev = null;
         }
 
         return root;
